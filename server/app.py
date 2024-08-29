8eb2fb0e76570afb114237a89c5df79c73200d65
Sebin <1219sebin@gmail.com>
2024-08-29 18:38:51 +0900

    나에게 보내기 성공

--- server/app.py
+++ server/app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') 
 def hello():
     return 'Hello, My First Flask!'
-  
-if __name__ == '__main__':
+
+if __name__ == '__main__':  
     app.run(port=3001)
